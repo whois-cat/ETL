2705faedef9ea260f2e270ff4d514a0959373d16
Anton M <anton2008m@gmail.com>
2021-12-26 19:48:18 +0500

    fix wrong path in creating indices

--- postgres_to_es/create_indexes.sh
+++ postgres_to_es/create_indexes.sh
@@ -1,3 +1,3 @@
-curl  -XPUT http://es:9200/movies -H 'Content-Type: application/json' -d @config.movies_schema.json &&
-curl  -XPUT http://es:9200/persons -H 'Content-Type: application/json' -d @config.persons_schema.json &&
-curl  -XPUT http://es:9200/genres -H 'Content-Type: application/json' -d @config.genres_schema.json
+curl  -XPUT http://es:9200/movies -H 'Content-Type: application/json' -d @config/movies_schema.json &&
+curl  -XPUT http://es:9200/persons -H 'Content-Type: application/json' -d @config/persons_schema.json &&
+curl  -XPUT http://es:9200/genres -H 'Content-Type: application/json' -d @config/genres_schema.json
